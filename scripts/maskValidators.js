57b56c30c61489189ef33f157747bca9ab73ada7
alexsanderarcelino <58044251+alexsanderarcelino@users.noreply.github.com>
2021-04-09 02:24:57 -0300

    Update maskValidators.js
    Atualização no código referente ao CNPJ

--- scripts/maskValidators.js
+++ scripts/maskValidators.js
@@ -1,12 +1,16 @@
 const Mask ={
-    cpfCnpj(input){
+    cpfCnpj(input) {
+
         let value = input.value;
-        value = value.replace(/\D/,"");
-        if(value.length > 14){
-            value = value.slice(0,-1);
+        value = value.replace(/\D/, "");
+
+        if (value.length > 14) {
+            value = value.slice(0, -1);
         }
+
         value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
         input.value = value;
+
     },
     cep(input){
         let value = input.value;
@@ -50,21 +54,20 @@ const Validate = {
         input.value = value;
 
     },
-    isCPF_CNPJ(input){
-        Validate.clearErrrors(input);
+    isCPF_CNPJ(input) {
+
+        Validate.clearErrors(input);
         let value = input.value;
         let error = null;
+        const cleanValues = value.replace(/\D/g, "");
 
-        const cleanValues = value.replace(/\D/g, '');
-        
-        if(cleanValues.length < 14 && cleanValues.length != 14){
+        if (cleanValues.length < 14 && cleanValues.length != 14) {
             error = "CNPJ incorreto";
-        }
-        
-        if(error){
             Validate.displayError(input, error);
         }
-        input.value = value;
+
+        input.Value = value;
+
     },
     isCEP(input){
         Validate.clearErrrors(input);

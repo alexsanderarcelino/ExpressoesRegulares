1fc84f79e0159db3f5321c7ebff02bc79593e6e0
alexsanderarcelino <58044251+alexsanderarcelino@users.noreply.github.com>
2021-04-08 21:43:15 -0300

    Atualização maskValidators.js
    Inserção dos códigos que fazem referência ao CPNJ.

--- scripts/maskValidators.js
+++ scripts/maskValidators.js
@@ -1,5 +1,15 @@
 
 const Mask ={
+    cpfCnpj(input) {
+        let value = input.value;
+        value = value.replace(/\D/, "");
+        if (value.length > 14) {
+            value = value.slice(0, -1);
+        }
+
+        value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+        input.value = value;
+    },
     cep(input){       
         let value = input.value;        
         value = value.replace(/\D/, "");        
@@ -12,6 +22,19 @@ const Mask ={
 }
 
 const Validate = {
+    isCPF_CNPJ(input) {
+        Validate.clearErrors(input);
+        let value = input.value;
+        let error = null;
+        const cleanValues = value.replace(/\D/g, '');
+        if (cleanValues.length < 14 && cleanValues.length != 14) {
+            error = "CPNJ incorreto";
+            Validate.displayError(input, error);
+        } else if (cleanValues.length < 12 && cleanValues.length != 11) {
+            error = "CPF incorreto";
+        }
+        input.value = value;
+    },
     isCEP(input){        
         Validate.clearErrors(input);        
         let value = input.value;       
